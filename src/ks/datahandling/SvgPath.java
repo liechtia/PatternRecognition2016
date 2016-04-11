6d29afc1556142a0f9fe85a7965121ee8e6db499
Maria Schmidt <maria_schmidt91@yahoo.de>
2016-04-11 11:45:06 +0200

    Extract word images
    Class WordImages: extracts all the word images
    Saved in KeywordSpottingData -> word images -> clipped/non_clipped
    non_clipped: extract rectangular area around the word
    clipped: extract exactly the box from the svg

---
+++ src/ks/datahandling/SvgPath.java
@@ -0,0 +1,40 @@
+package ks.datahandling;
+
+public class SvgPath {
+
+    private int[] xValues;
+    private int[] yValues;
+    private int height;
+    private int width; 
+    private int startX;
+    private int startY;
+ 
+    
+    public int[] getXValues() { return this.xValues;}
+    public int[] getYValues() { return this.yValues;}
+    public int getHeight() {return this.height;}
+    public int getWidth() { return this.width;}
+    public int getStartX() {return this.startX;}
+    public int getStartY() { return this.startY;}
+    
+    public SvgPath(int height, int width, int[] xValues, int[] yValues) {
+        this.height = height;
+        this.width = width;
+        this.xValues = xValues;
+        this.yValues = yValues;
+    }
+    
+    public void normValues(int startX, int startY)
+    {
+        this.startX = startX;
+        this.startY = startY; 
+        
+        for(int i = 0; i < xValues.length; i++)
+        {
+            xValues[i] = xValues[i]-startX;
+            yValues[i] = yValues[i] -startY;
+        }
+    }
+ 
+
+}
---
+++ src/main/mainKeywordSpottingData.java
@@ -0,0 +1,21 @@
+//
+//
+
+package main;
+
+import ks.datahandling.WordImages;
+
+public class mainKeywordSpottingData {
+
+    private static final String folderLocations = "KeywordSpottingData/ground-truth/locations";
+    private static final String folderSave = "KeywordSpottingData/wordimages";
+    private static final String folderImages = "KeywordSpottingData/images";
+    private static final String transcriptionFile = "KeywordSpottingData/ground-truth/transcription.txt";
+    
+    
+    public static void main(String[] args) {
+        WordImages wi = new WordImages( folderImages, folderLocations, folderSave,  transcriptionFile);
+        wi.getWordImages();
+    }
+
+}
