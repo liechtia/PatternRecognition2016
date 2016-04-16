889de049508888640b4dfcfd2799f7d15b8fd2f9
Andrea Liechti <andrea_liechti@msn.com>
2016-04-16 14:09:34 +0200

    Binarized images with binarization class

---
+++ src/main/mainBinarization.java
@@ -0,0 +1,17 @@
+package main;
+
+import ks.datahandling.Binarization;
+
+public class mainBinarization {
+    private static final String folderClipped = "KeywordSpottingData/wordimages/clipped/";
+    private static final String folderClippedSave = "KeywordSpottingData/wordimages/clipped_binary/";
+    private static final String folderNonClipped = "KeywordSpottingData/wordimages/non_clipped/";
+    private static final String folderNonClippedSave = "KeywordSpottingData/wordimages/non_clipped_binary/";
+
+
+    public static void main(String[] args) {
+        Binarization binaryConverter = new Binarization();
+        binaryConverter.binarizeImages(folderClipped, folderClippedSave, 100);
+        binaryConverter.binarizeImages(folderNonClipped, folderNonClippedSave, 175);
+    }
+}
